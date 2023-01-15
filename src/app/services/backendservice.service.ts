e331a17edf85f3d05111aa8c306a59b12a898429
MatthieuOlenine <you@example.com38001232@parisnanterre.fr>
2023-01-15 09:48:17 +0100

    merge conflict

--- src/app/services/backendservice.service.ts
+++ src/app/services/backendservice.service.ts
@@ -14,11 +14,7 @@ export class BackendserviceService {
   constructor(private http : HttpClient) {}
 
   addteam(teamname : any){
-<<<<<<< HEAD
     return this.http.post("http://localhost:4815/api/register", {
-=======
-    return this.http.post("http://hunterxnanterreserver.com:4815/api/register", {
->>>>>>> 635c7b4361d80beac4e1ec29f8566a8cacc485c7
       teamname,
       red_door: "close",
       blue_door: "close",
@@ -33,11 +29,7 @@ export class BackendserviceService {
   }
 
   getteamdata(): Observable<Data[]>{
-<<<<<<< HEAD
     return this.http.get<Data[]>("http://localhost:4815/api/data").pipe(
-=======
-    return this.http.get<Data[]>("http://hunterxnanterreserver.com:4815/api/data").pipe(
->>>>>>> 635c7b4361d80beac4e1ec29f8566a8cacc485c7
       catchError((err) => {
         this.errMsg = err;
         return of([])
@@ -46,11 +38,7 @@ export class BackendserviceService {
   }
 
   getteamdataone(item:any): Observable<Data[]>{
-<<<<<<< HEAD
     return this.http.get<Data[]>(`http://localhost:4815/api/dataone/${item}`).pipe(
-=======
-    return this.http.get<Data[]>(`http://hunterxnanterreserver.com:4815/api/dataone/${item}`).pipe(
->>>>>>> 635c7b4361d80beac4e1ec29f8566a8cacc485c7
       catchError((err) => {
         this.errMsg = err;
         return of([])
@@ -59,11 +47,7 @@ export class BackendserviceService {
   }
 
   login(teamname:any, password:any){
-<<<<<<< HEAD
     return this.http.get<{name:any}>(`http://localhost:4815/api/logindata/${teamname}/${password}`).pipe(
-=======
-    return this.http.get<{name:any}>(`http://hunterxnanterreserver.com:4815/api/logindata/${teamname}/${password}`).pipe(
->>>>>>> 635c7b4361d80beac4e1ec29f8566a8cacc485c7
       catchError((err) => {
         this.errMsg = err;
         return of([])
@@ -72,11 +56,7 @@ export class BackendserviceService {
   }
 
   logincolor(teamname:any, password:any, color:any){
-<<<<<<< HEAD
     return this.http.put('http://localhost:4815/api/logincolordata', {
-=======
-    return this.http.put('http://hunterxnanterreserver.com:4815/api/logincolordata', {
->>>>>>> 635c7b4361d80beac4e1ec29f8566a8cacc485c7
       name : teamname,
       pass : password,
       color : color
@@ -84,11 +64,7 @@ export class BackendserviceService {
   }
 
   submissioninput(sub:any, name:any){
-<<<<<<< HEAD
     return this.http.get<{resforsub:any}>(`http://localhost:4815/api/sub/${sub}/${name}`).pipe(
-=======
-    return this.http.get<{resforsub:any}>(`http://hunterxnanterreserver.com:4815/api/sub/${sub}/${name}`).pipe(
->>>>>>> 635c7b4361d80beac4e1ec29f8566a8cacc485c7
       catchError((err) => {
         this.errMsg = err;
         return of([])
@@ -97,11 +73,7 @@ export class BackendserviceService {
   }
 
   gettotal(){
-<<<<<<< HEAD
     return this.http.get<{leng:any}>("http://localhost:4815/api/total").pipe(
-=======
-    return this.http.get<{leng:any}>("http://hunterxnanterreserver.com:4815/api/total").pipe(
->>>>>>> 635c7b4361d80beac4e1ec29f8566a8cacc485c7
       catchError((err) => {
         this.errMsg = err;
         return of([])
@@ -110,18 +82,10 @@ export class BackendserviceService {
   }
 
   initialone(id:any){
-<<<<<<< HEAD
     return this.http.delete(`http://localhost:4815/api/initone/${id}`).subscribe()
   }
 
   initial(){
     return this.http.delete("http://localhost:4815/api/init").subscribe()
-=======
-    return this.http.delete(`http://hunterxnanterreserver.com:4815/api/initone/${id}`).subscribe()
-  }
-
-  initial(){
-    return this.http.delete("http://hunterxnanterreserver.com:4815/api/init").subscribe()
->>>>>>> 635c7b4361d80beac4e1ec29f8566a8cacc485c7
   }
 }
